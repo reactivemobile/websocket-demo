d3691058aa016586dcdc5ab26d5777e833f15743
reactivemobile <donal@reactivemobile.com>
2021-01-08 13:28:48 +0000

    Better js layout

--- web/drawing_script.js
+++ web/drawing_script.js
@@ -1,75 +1,83 @@
 function connect(ip_address) {
 
     var canvas = document.getElementById('canvas');
+    var status = document.getElementById('status');
 
     if (canvas.getContext) {
+        var context = canvas.getContext('2d');
+
+        status.innerHTML = "Connecting";
         var webSocket = new WebSocket("ws://" + ip_address + ":8080/");
 
-        var context = canvas.getContext('2d');
-        var status = document.getElementById('status')
-        status.innerHTML = "Connecting";
-
-        webSocket.onopen = function(frame) {
-            status.innerHTML = "Connected";
-            clearCanvas();
-        };
-
-        var indexOfFrames = 0
-        var previousX = 0
-        var previousY = 0
-
-        var phoneCanvasWidth = 0
-        var phoneCanvasHeight = 0
-
+        var lineIndex = 0
         var scale = 0
 
+        webSocket.onopen = function(frame) {
+            status.innerHTML = "Connected to "+ip_address + ":8080";
+        };
+
         webSocket.onmessage = function(frame) {
-            var content = frame.data
-
-            if (content.startsWith("start")) {
-                var deviceCanvasSize = content.split(",");
-                var width = deviceCanvasSize[1];
-                var height = deviceCanvasSize[2];
-                console.log("Device canvas is " + width + " x " + height);
-                scale = Math.min(width / parent.innerWidth, height / parent.innerHeight) / 4
-
-                canvas.width = width * scale;
-                canvas.height = height * scale;
-
-                clearCanvas()
-
-                return
-            }
-
-            if (content == "clear") {
-                clearCanvas();
-                indexOfFrames = 0;
-                return;
-            }
-
-            if (content == "pointer_lifted") {
-                indexOfFrames = 0;
-                return;
-            }
-
-            var point = content.split(",")
-            var x = point[0] * scale
-            var y = point[1] * scale
-
-            if (indexOfFrames == 0) {
-                context.beginPath();
-                context.moveTo(x, y)
-            } else {
-                context.lineTo(x, y);
-                context.stroke()
-            }
-
-            indexOfFrames++
+            handleFrame(frame)
         }
+
         webSocket.onclose = function(frame) {
-            status.innerHTML = "Connection closed";
+            status.innerHTML = "Disconnected";
         };
     };
+
+    function handleFrame(frame) {
+
+        var content = frame.data
+
+        if (content.startsWith("start")) {
+            handleStartFrame(content)
+            return
+        }
+
+        if (content == "clear") {
+            clearCanvas();
+            lineIndex = 0;
+            return;
+        }
+
+        if (content == "pointer_lifted") {
+            lineIndex = 0;
+            return;
+        }
+
+        var point = content.split(",")
+        var x = point[0] * scale
+        var y = point[1] * scale
+
+        if (lineIndex == 0) {
+            context.beginPath();
+            context.moveTo(x, y)
+        } else {
+            context.lineTo(x, y);
+            context.stroke()
+        }
+
+        lineIndex++
+    }
+
+    // Scale the canvas to match ratio of device screen
+    function handleStartFrame(content) {
+        var deviceCanvasSize = content.split(",");
+        var width = deviceCanvasSize[1];
+        var height = deviceCanvasSize[2];
+        console.log("Device canvas is " + width + " x " + height);
+
+        if (parent.innerHeight < height) {
+            scale = Math.min(parent.innerWidth / width, parent.innerHeight / height) / 2
+        } else {
+            scale = Math.min(width / parent.innerWidth, height / parent.innerHeight) / 2
+        }
+
+        canvas.width = width * scale;
+        canvas.height = height * scale;
+
+        clearCanvas()
+    }
 
     function clearCanvas() {
         context.clearRect(0, 0, canvas.width, canvas.height);
